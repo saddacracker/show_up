c50b7930ee6f270b661a8399777514d5ad81982a
John Cardwell <cardwell.john@gmail.com>
2013-04-05 23:38:01 -0700

    add seed data and check for number on time fields

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,11 +1,12 @@
 class Meeting < ActiveRecord::Base
   attr_accessible :address, :closed_meeting, :days_of_week, :description, :duration, :latitude, :longitude, :tags, :time, :title
   
+  validates_presence_of :address, :duration, :days_of_week, :tags, :time, :title
+  validates_numericality_of :time, :duration
+  
   serialize :days_of_week , Hash
   serialize :tags         , Hash
-  
-  validates_presence_of :address, :duration, :days_of_week, :tags, :time, :title
-  
+
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# create a user
+User.create({ :email => 'admin@example.com', :password => 'password', :password_confirmation => 'password'})
+
+
+#create some meetings
+meetings = Meeting.create([
+{:address => '99 Ferry St. Easthampton, MA', :days_of_week => {"mon"=>"1", "tue"=>"0", "wed"=>"0", "thu"=>"0", "fri"=>"0", "sat"=>"0", "sun"=>"0"}, :duration => 2, :time => 19, :title => 'Test Meeting', :tags => {"speaker"=>"1", "gay"=>"0", "childcare"=>"0", "mens"=>"0", "womens"=>"1", "book_study"=>"0", "step_study"=>"0"}},  
+
+{:address => '1827 McDougall Ave. Everett, WA', :days_of_week => {"mon"=>"1", "tue"=>"0", "wed"=>"0", "thu"=>"0", "fri"=>"0", "sat"=>"0", "sun"=>"0"}, :duration => 1, :time => 19, :title => 'New Life', :tags => {"speaker"=>"1", "gay"=>"0", "childcare"=>"0", "mens"=>"1", "womens"=>"0", "book_study"=>"0", "step_study"=>"0"}},
+
+{:address => '5024 161st PL SW Edmonds, WA', :days_of_week => {"mon"=>"1", "tue"=>"0", "wed"=>"0", "thu"=>"0", "fri"=>"0", "sat"=>"0", "sun"=>"0"}, :duration => 1, :time => 19, :title => '13th Step Group', :tags => {"speaker"=>"1", "gay"=>"0", "childcare"=>"1", "mens"=>"0", "womens"=>"0", "book_study"=>"0", "step_study"=>"0"}},
+
+{:address => 'Everett and Hoyt Everett, WA', :days_of_week => {"mon"=>"0", "tue"=>"0", "wed"=>"0", "thu"=>"1", "fri"=>"0", "sat"=>"0", "sun"=>"0"}, :duration => 1, :time => 19, :title => 'A Vision For Yo Mammy', :tags => {"speaker"=>"1", "gay"=>"0", "childcare"=>"0", "mens"=>"0", "womens"=>"0", "book_study"=>"0", "step_study"=>"0"}}
+])
