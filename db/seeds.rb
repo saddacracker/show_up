424f7fc1b6366395edd851cd25494d17621346f5
John Cardwell <cardwell.john@gmail.com>
2013-04-08 17:17:48 -0700

    update seeds and add better time and duration pickers

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,7 @@ User.create({ :email => 'admin@example.com', :password => 'password', :password_
 
 #create some meetings
 meetings = Meeting.create([
-{:address => '99 Ferry St. Easthampton, MA', :days_of_week => {"mon"=>"1", "tue"=>"0", "wed"=>"0", "thu"=>"0", "fri"=>"0", "sat"=>"0", "sun"=>"0"}, :duration => 2, :time => 19, :title => 'Test Meeting', :tags => {"speaker"=>"1", "gay"=>"0", "childcare"=>"0", "mens"=>"0", "womens"=>"1", "book_study"=>"0", "step_study"=>"0"}},  
+{:address => '99 Ferry St. Easthampton, MA', :week_days => {"friday"=>"0", "monday"=>"0", "sunday"=>"0", "tuesday"=>"0", "saturday"=>"0", "thursday"=>"1", "wednesday"=>"1"}, :duration => 100, :time => 1200, :title => 'Sample Meeting', :tags => {"speaker"=>"1", "childcare"=>"0", "book_study"=>"0"}},
 
-{:address => '1827 McDougall Ave. Everett, WA', :days_of_week => {"mon"=>"1", "tue"=>"0", "wed"=>"0", "thu"=>"0", "fri"=>"0", "sat"=>"0", "sun"=>"0"}, :duration => 1, :time => 19, :title => 'New Life', :tags => {"speaker"=>"1", "gay"=>"0", "childcare"=>"0", "mens"=>"1", "womens"=>"0", "book_study"=>"0", "step_study"=>"0"}},
-
-{:address => '5024 161st PL SW Edmonds, WA', :days_of_week => {"mon"=>"1", "tue"=>"0", "wed"=>"0", "thu"=>"0", "fri"=>"0", "sat"=>"0", "sun"=>"0"}, :duration => 1, :time => 19, :title => '13th Step Group', :tags => {"speaker"=>"1", "gay"=>"0", "childcare"=>"1", "mens"=>"0", "womens"=>"0", "book_study"=>"0", "step_study"=>"0"}},
-
-{:address => 'Everett and Hoyt Everett, WA', :days_of_week => {"mon"=>"0", "tue"=>"0", "wed"=>"0", "thu"=>"1", "fri"=>"0", "sat"=>"0", "sun"=>"0"}, :duration => 1, :time => 19, :title => 'A Vision For Yo Mammy', :tags => {"speaker"=>"1", "gay"=>"0", "childcare"=>"0", "mens"=>"0", "womens"=>"0", "book_study"=>"0", "step_study"=>"0"}}
+{:address => '45 Mereline Ave. Eastlongmeadow, MA', :week_days => {"friday"=>"1", "monday"=>"1", "sunday"=>"1", "tuesday"=>"0", "saturday"=>"0", "thursday"=>"1", "wednesday"=>"1"}, :duration => 100, :time => 1200, :title => 'Sample Meeting 2', :tags => {"speaker"=>"1", "childcare"=>"1", "book_study"=>"1"}}
 ])
