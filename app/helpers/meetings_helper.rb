f9b5f112d9a86bbfa4f194b301ff407fe92bb6c0
John Cardwell <cardwell.john@gmail.com>
2013-04-09 11:27:15 -0700

    rename time array in meeting helper

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -2,7 +2,7 @@ module MeetingsHelper
   
   def am_pm_hour_select(object, method, options = {}, html_options = {})
     select_options = create_hours(:start_time => 5.hours, :end_time => 23.hours)
-    # select_options = [ ["6am", 6], ["7am", 7], ["8am", 8], ["9am", 9], ["10am", 10], ["11am", 11], ["12pm", 12], ["1pm", 13], ["2pm", 14], ["3pm", 15], ["4pm", 16], ["5pm", 17], ["6pm", 18], ["7pm", 19], ["8pm", 20], ["9pm", 21], ["10pm", 22], ["11pm", 23], ["12am", 24], ["1am", 1], ["2am", 2], ["3am", 3], ["4am",4 ], ["5am", 5]]
+
     unless options[:start].nil?
        shift_if_needed = Proc.new{|hour, start| hour<start ? hour+24 : hour}
        select_options.sort!{|x, y| shift_if_needed.call(x.last,options[:start]) <=> shift_if_needed.call(y.last, options[:start]) }
@@ -20,20 +20,15 @@ module MeetingsHelper
     Array.new(1 + (end_time - start_time)/increment) do |i|
       ampm_format     = (Time.now.midnight + (i*increment) + start_time).strftime("%I:%M %p") 
       military_format = (Time.now.midnight + (i*increment) + start_time).strftime("%H%M")
-      # Array.new( ampm_format + ", " + military_format )
-      array = Array.new
-      array.push ampm_format
-      array.push military_format.to_i
       
-      array
+      time_options = Array.new
+      time_options.push ampm_format
+      time_options.push military_format.to_i
+      
+      time_options
     end
     
-    # ["05:30 AM", "06:00 AM", "06:30 AM", "07:00 AM", "07:30 AM", "08:00 AM", 
-    #     "08:30 AM", "09:00 AM", "09:30 AM", "10:00 AM", "10:30 AM", "11:00 AM", 
-    #     "11:30 AM", "12:00 PM", "12:30 PM", "01:00 PM", "01:30 PM", "02:00 PM", 
-    #     "02:30 PM", "03:00 PM", "03:30 PM", "04:00 PM", "04:30 PM", "05:00 PM", 
-    #     "05:30 PM", "06:00 PM", "06:30 PM", "07:00 PM", "07:30 PM", "08:00 PM", 
-    #     "08:30 PM", "09:00 PM", "09:30 PM", "10:00 PM", "10:30 PM", "11:00 PM"]
+    # [ ["7:00 PM", 1900], ["7:30 PM", 1930], ["7:45 PM", 1945], ["8:00 PM", 2000] ]
   end
   
 end
