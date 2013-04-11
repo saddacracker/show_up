02d2c602b6bd46b4f79119803133d77a1ebf44d4
John Cardwell <cardwell.john@gmail.com>
2013-04-10 21:00:02 -0700

    add dynamic titles and meta tags

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 ShowUp::Application.initialize!
+
+# Load the default title. Er ma gerd
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/title.yml")
