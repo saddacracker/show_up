9a1defeceec6f7c5bbb804780c018597dd29d534
John Cardwell <cardwell.john@gmail.com>
2013-08-12 16:26:36 -0700

    test ruby from bash

--- lib/tasks/imgur_upload.rb
+++ lib/tasks/imgur_upload.rb
@@ -12,5 +12,8 @@ def imgur(key, cookie, file_path)
   response   = RestClient.post(url, data, headers)
   json       = JSON.parse(response.body)["rsp"]["image"] rescue nil
 end
+# key, IMGURSESSION cookie value, filename
+imgur("f01885c6a958e51", "jtrkdk1vuifopiciil0gq11er6", "~/Desktop/commitphotos/studmuffin.png")
 
-imgur("your-key", "your-cookie", "/images/me.png")
+# f01885c6a958e51 - client id
+# f47a796f046d4502fdf164391b9e2f43795d520e - client secret
