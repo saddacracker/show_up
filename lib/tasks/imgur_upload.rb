0fc349233114d85061859aad466b27df414f7737
John Cardwell <cardwell.john@gmail.com>
2013-08-14 21:54:01 -0700

    modify upload script to inclde commit message bodytake two

--- lib/tasks/imgur_upload.rb
+++ lib/tasks/imgur_upload.rb
@@ -13,3 +13,18 @@ COMMIT_MSG=ARGV[1]
 flickr.upload_photo PHOTO_PATH, :title => COMMIT_MSG, :description => "Whenever I commit code to my project, you get a snapshot"
 
 puts "photo uploaded to flickr [()]"
+
+list   = flickr.photos.getRecent
+
+id     = list[0].id
+secret = list[0].secret
+info = flickr.photos.getInfo :photo_id => id, :secret => secret
+
+puts info.title           # => "PICT986"
+puts info.dates.taken     # => "2006-07-06 15:16:18"
+
+sizes = flickr.photos.getSizes :photo_id => id
+
+original = sizes.find {|s| s.label == 'Original' }
+puts original.width       # => "800" -- may fail if they have no original marked image
+puts original.url
