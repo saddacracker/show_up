c9d14be2011083051f600b17efaf95b95e46c869
John Cardwell <cardwell.john@gmail.com>
2013-08-14 17:02:16 -0700

    change teh padding

--- lib/tasks/imgur_upload.rb
+++ lib/tasks/imgur_upload.rb
@@ -26,7 +26,7 @@ puts "Copy here the number given when you complete the process."
 verify = gets.strip
 
 begin
-  flickr.get_access_token(token['oauth_token'], token['oauth_token_secret'], verify)
+  flickr.get_access_token(token['72157635070493099-767dcffa71507a82'], token['26-045-517'], verify)
   login = flickr.test.login
   puts "You are now authenticated as #{login.username} with token #{flickr.access_token} and secret #{flickr.access_secret}"
 rescue FlickRaw::FailedResponse => e
