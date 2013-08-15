98ac895f8ead83e55a17d75e7d937f6dab1ecfe4
John Cardwell <cardwell.john@gmail.com>
2013-08-14 21:07:27 -0700

    modify upload script to inclde commit message

--- lib/tasks/imgur_upload.rb
+++ lib/tasks/imgur_upload.rb
@@ -7,6 +7,7 @@ flickr.access_token = "72157635073309787-21992ca11007e052"
 flickr.access_secret = "5bf335259627714b"
 
 PHOTO_PATH=ARGV[0]
+COMMIT_MSG=ARGV[1]
 
 # You need to be authentified to do that, see the previous examples.
-flickr.upload_photo PHOTO_PATH, :title => "I totes just committed", :description => "Whenever I commit code to my project, you get a snapshot"
+flickr.upload_photo PHOTO_PATH, :title => COMMIT_MSG, :description => "Whenever I commit code to my project, you get a snapshot"
