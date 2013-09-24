fe72872eea114fca8c281fe0eff953e9f5a3f428
John Cardwell <cardwell.john@gmail.com>
2013-09-23 21:22:22 -0700

    test scrape

--- test.rb
+++ test.rb
@@ -2,6 +2,26 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-url = "http://seattleaa.org/directory/outputt3x.php"
-doc = Nokogiri::HTML(open(url))
-puts doc.at_css("title").text
+days = %w[sunday monday tuesday wednesday thursday friday saturday]
+
+days.each do |day|
+  url = "http://seattleaa.org/directory/web#{day}.html"
+  doc = Nokogiri::HTML(open(url))
+
+  doc.css('table tr').each do |item|
+    # puts item.content
+    if  item.at_css("td")
+      division  = item.at_css("td").text
+      time      = item.at_css("td + td").text
+      is_closed = item.at_css("td + td + td").text
+      name      = item.at_css("td + td + td + td").text.capitalize
+      address   = item.at_css("td + td + td + td + td").text
+      tags      = item.at_css("td + td + td + td + td + td").text 
+
+      puts "#{time} - #{name} - #{address} - #{is_closed} - #{division} - #{tags}"
+    end
+  end
+end
+
+
+
