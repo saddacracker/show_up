f5a62090fd38e181e59a2f6abbc5d5cd549ebf93
John Cardwell <cardwell.john@gmail.com>
2013-11-02 17:42:56 -0700

    create queries for updating and creating meetings (in comments)

--- test.rb
+++ test.rb
@@ -21,6 +21,50 @@ def clean_the_time(timey)
   military_format = timey.strftime("%H%M")
 end
 
+def convert_tags(tags)
+  # an at cc gs mo oh si sp ss wb we wo wp yp
+  # unless tags.nil?
+  #     tags = tags[1..-2].split(' ').collect! {|n| n} 
+  #   end
+end
+
+def convert_halls(hall)
+  # Alano Club Of The Eastside 
+  # 12302 NE 8th, Bellevue 98005
+  
+  # Capital Hill Alano Club
+  
+  # Ed-Lynn Fellowship Hall
+  # 5800 198th SW, Suite 1, Lynnwood 98036
+  
+  # Fremont Fellowship Hall
+  # 8916 Aurora Avenue North, Seattle 98103
+  
+  # Lynnwood Alano Club
+  # 4001 198th Street SW, Lynnwood 98036
+  
+  # A New Beginning
+  # 15403 Ambaum Blvd S.W., Burien 98166
+  
+  # The Phoenix Club
+  # 15011 Aurora Ave N, Seattle
+  
+  # Serenity Hall
+  # 12536 Renton Ave South, Renton 98178
+  
+  # Groups at the South County Alano Club
+  # 1317 Harvey Road, Auburn 98002
+  
+  # Southend Fellowship 
+  # 321 - 3rd Ave South, Kent 98032
+  
+  # 12 & 12 Fellowship
+  # 23732 Bothell-Everett Highway
+  
+  # Cherry Fellowship
+  # 2701 East Cherry, Seattle 98122
+end
+
 days = %w[sunday monday tuesday wednesday thursday friday saturday]
 number = 0
 
@@ -28,12 +72,12 @@ days.each do |day|
   url = "http://seattleaa.org/directory/web#{day}.html"
   doc = Nokogiri::HTML(open(url))
   
-  puts "\n ::::::: #{day.capitalize} ::::::: \n\n"
+  # puts "\n ::::::: #{day.capitalize} ::::::: \n\n"
 
   doc.css('table tr').each do |item|
-    # @TODO: convert halls to addresses
-    # @TODO: convert their tags to my tag and create hash (hstore compliant)
-    # @TODO: check for matching title (update day of week hash, if so)
+    # 1) @TODO: convert clubs and halls to addresses
+    # 3) @TODO: convert their tags to my tag and create hash (hstore compliant)
+    # 2) @TODO: check for matching title (update day of week hash, if so)
     
     if  item.at_css("td")
       division  = item.at_css("td").text
@@ -51,13 +95,29 @@ days.each do |day|
         duration = 60
       end
       
+      # convert the rest of the tags
+      convert_tags(tags)
+      
       # convert is_closed
       is_closed == "C" ? is_closed = true : is_closed = false
-        
       
       number = number+1
-
-      puts "#{number} #{division} : #{time} - #{name} - #{address} - #{is_closed} - #{tags} - #{duration}"
+      
+      # 2) @TODO: check for matching title (update day of week hash, if so)
+      # make an array of titles
+      
+      
+      # if it matches, update with new day of week
+      # http://apidock.com/rails/ActiveRecord/Relation/update_all
+      # Meeting.where('title LIKE ?', "%#{name}%").update_all(:week_days => {:friday => 0, :monday => 0, :sunday => 0, :tuesday => 0, :saturday => 0, :thursday => 0, :wednesday => 0})
+      
+      # if it doesn't match, create
+      # http://ruby.jieck.com/blog/2013/03/19/nosqlize-postgresql-with-hstore-in-rails-3.2plus/
+      puts "CREATING: #{number} #{division} : #{time} - #{name} - #{address} - #{is_closed} - #{tags} - #{duration}"
+      
+      # Meeting.create(:address => address, :week_days => {:friday => 0, :monday => 0, :sunday => 0, :tuesday => 0, :saturday => 0, :thursday => 0, :wednesday => 0}, :duration => duration, :time => time, :closed_meeting => is_closed, :description => "No Description", :title => name, :tags => {})
+      
+      
     end
   end
 end
