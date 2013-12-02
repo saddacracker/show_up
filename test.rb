38bfae5aff9199c18e0ac06b948934fdf77a6c87
John Cardwell <cardwell.john@gmail.com>
2013-12-02 13:03:15 -0800

    update hash of addresses

--- test.rb
+++ test.rb
@@ -4,7 +4,26 @@ require 'open-uri'
 require 'time'
 
 @meetings = Array.new 
-@halls = Array.new
+@halls = {
+  "Ed Lynn Fellowship" => "5800 198th SW, Suite 1, Lynnwood 98036",
+  "Pass It On - 17801 1st Ave S 98148" => "17801 1st Ave S 98148",
+  "Serenity Hall" => "12536 Renton Ave South, Renton 98178",
+  "Southend Fellowship" => "321 3rd Ave South, Kent 98032",
+  "12 & 12 Fellowship" => "23732 Bothell Everett Hwy Bothell, WA 98021",
+  "Pass It On -17801 1st Ave S 98148" => "17801 1st Ave S 98148",
+  "A New Beginning" => "15403 Ambaum Blvd S.W., Burien 98166",
+  "Lynnwood Alano Club" => "4001 198th Street SW, Lynnwood 98036",
+  "Phoenix Club" => "15011 Aurora Ave N, Seattle",
+  "Fremont Fellowship Hall" => "8916 Aurora Avenue North, Seattle 98103",
+  "Cherry Fellowship" => "2701 East Cherry, Seattle 98122",
+  "South King Alano Club" => "1317 Harvey Road, Auburn 98002",
+  "South County Alano Club" => "1317 Harvey Road, Auburn 98002",
+  "Alano Club of the Eastside" => "12302 NE 8th, Bellevue 98005",
+  "Eastside Alano Club" => "12302 NE 8th, Bellevue 98005",
+  "12 & 12 Fellowship" => "23732 Bothell-Everett Highway",
+  "12 & 12 Study" => "23732 Bothell-Everett Highway",
+  "12 & 12 FELLOWSHIP" => "23732 Bothell-Everett Highway",
+  }
 
 def change_the_case(name)
   name.downcase
@@ -29,7 +48,7 @@ def convert_tags(tags)
   #   end
 end
 
-def convert_hall(hall)
+def convert_empty_hall(hall)
   
   # Alano Club Of The Eastside 
   # 12302 NE 8th, Bellevue 98005
@@ -67,24 +86,48 @@ def convert_hall(hall)
   # 2701 East Cherry, Seattle 98122
   
   
-  # list of halls
-  # Ed Lynn Fellowship
-  #   Pass It On - 17801 1st Ave S 98148
-  #   Serenity Hall
-  #   Southend Fellowship
-  #   12 & 12 Fellowship
-  #   Pass It On -17801 1st Ave S 98148
-  #   A New Beginning
+  # adding addresses to halls missing addresses from the scraped website  
+  # correct_halls = [{
+  #   "Ed Lynn Fellowship" => "5800 198th SW, Suite 1, Lynnwood 98036",
+  #   "Pass It On - 17801 1st Ave S 98148" => "17801 1st Ave S 98148",
+  #   "Serenity Hall" => "12536 Renton Ave South, Renton 98178",
+  #   "Southend Fellowship" => "321 3rd Ave South, Kent 98032",
+  #   "12 & 12 Fellowship" => "23732 Bothell Everett Hwy Bothell, WA 98021",
+  #   "Pass It On -17801 1st Ave S 98148" => "17801 1st Ave S 98148",
+  #   "A New Beginning" => "15403 Ambaum Blvd S.W., Burien 98166",
+  #   }]
+  
+  # unless @halls.include? hall
+  #   @halls << hall
+  #   print hall
+  # end
   
   hall = hall.strip
   
-  if @halls.include? hall
-    
-  else
-    @halls << hall
-    print hall
-  end
+  return @halls[hall]
   
+  
+end
+
+def convert_hall_to_address(hall)
+  
+  # Lynnwood Alano Club
+  # Phoenix Club
+  # Fremont Fellowship Hall
+  # Cherry Fellowship
+  # South King Alano Club
+  # South County Alano Club
+  # Alano Club of the Eastside
+  # Eastside Alano Club
+  # 12 & 12 Fellowship
+  # 12 & 12 Study
+  # 12 & 12 FELLOWSHIP
+  # South County Alano Club
+  # South King Alano Club
+  
+  hall = hall.strip
+  
+  return @halls[hall] 
   
 end
 
@@ -125,27 +168,33 @@ days.each do |day|
       is_closed == "C" ? is_closed = true : is_closed = false
       
       if address.empty?
-        convert_hall(name)
+        address = convert_empty_hall(name)
       end
+      
+      # if @halls.key(address)
+      #   puts address
+      #   # address = convert_hall_to_address(address)
+      #   # puts address
+      # end
       
       number = number+1
 
       # puts "CREATING: #{number} #{division} : #{time} - #{name} - #{address} - #{is_closed} - #{tags} - #{duration}"
       
       if @meetings.include? name
-        print "-"
+        puts "UPDATING: #{address}"
         # http://apidock.com/rails/ActiveRecord/Relation/update_all
         # Meeting.where('title LIKE ?', "%#{name}%").update(:week_days => {:friday => 0, :monday => 0, :sunday => 0, :tuesday => 0, :saturday => 0, :thursday => 0, :wednesday => 0})
         
         # temp_meetings = Meeting.where('title LIKE ?', "%#{name}%")
         #         # m = m.first
         #         temp_meetings.each do |m|
-        #           m.update_attribute :friday, 1
+        #           m.update_attribute day.to_sym, 1
         #         end
         
       
       else 
-        print "."
+        puts "CREATING: #{address}"
         @meetings << name
         #        Meeting.create(:address => address, :week_days => {day => 1}, :duration => duration, :time => time, :closed_meeting => is_closed, :description => "No Description", :title => name, :tags => {})
         
