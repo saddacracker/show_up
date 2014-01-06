c4c3feea569889d5ae8d745420fe3bb45f767d25
John Cardwell <cardwell.john@gmail.com>
2014-01-05 21:58:06 -0800

    remove escape_js helper

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -141,13 +141,3 @@ class MeetingsController < ApplicationController
     end
   end
 end
-
-
-class Helper
-  include ActionView::Helpers::JavaScriptHelper
-
-  def self.escape_js( text )
-    @instance ||= self.new
-    return @instance.escape_javascript( text )
-  end
-end
